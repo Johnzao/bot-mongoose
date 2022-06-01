49e1e9fef5a9cfdba53a3734c31f54a3933d537d
Johnzao <46110362+Johnzao@users.noreply.github.com>
2022-06-01 12:08:43 -0300

    Update readyListeners.js

--- src/events/readyListeners.js
+++ src/events/readyListeners.js
@@ -16,9 +16,6 @@ module.exports = class {
 
         }
         setInterval(updatedRichPresence, 1000 * 10);
-        this.client.functions.checkMessages();
-        this.client.functions.checkTickets();
-        this.client.functions.updateMessages();
         let time = (parseFloat(finishedAt - this.client.startedAt).toFixed(2)).replace(".00", "");
         console.log(`\x1b[38;5;75m✔ [${this.client.user.username}] Conexão com o Discord efetuada em ${time}ms\x1b[0m`);
     } catch(error) {
